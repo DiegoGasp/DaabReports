4894c66cab1487cc8a2c077fcfef67c3302adcfd
DiegoGasp <diego@daabcontech.com>
2025-09-25 10:09:10 -0600

    Fix Navisworks API usage in export plugin

---
+++ DaabNavisExport/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DaabNavisExport")]
+[assembly: AssemblyDescription("Navisworks 2026 export automation for Daab Reports")]
+[assembly: AssemblyCompany("Daab Reports")]
+[assembly: AssemblyProduct("DaabNavisExport")]
+[assembly: AssemblyCopyright("Copyright © Daab Reports")]
+[assembly: ComVisible(false)]
+[assembly: Guid("e324e173-2803-489b-b727-34a96e616d67")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
